4f29c167613e0ef37a6e928b1a1475926720fe20
Arpit Verma <131401678+vermaArpitt@users.noreply.github.com>
2024-10-21 15:53:50 +0530

    Update game_window.py
    Added all 195 flags to the game.

--- game_window.py
+++ game_window.py
@@ -10,10 +10,201 @@ def loadFlags(dic):
         Flags[key] = p.image.load("flags/" + key + ".png")
 
 def game_loop(screen):
-    dic = {'in' : 'India',
+    dic = {'af' : 'Afghanistan',
+           'al' : 'Albania',
+           'dz' : 'Algeria',
+           'ad' : 'Andorra',
+           'ao' : 'Angola',
+           'ag' : 'Antigua and Barbuda',
+           'ar' : 'Argentina',
+           'am' : 'Armenia',
+           'au' : 'Australia',
+           'at' : 'Austria',
+           'az' : 'Azerbaijan',
+           'bs' : 'Bahamas',
+           'bh' : 'Bahrain',
+           'bd' : 'Bangladesh',
+           'bb' : 'Barbados',
+           'by' : 'Belarus',
+           'be' : 'Belgium',
+           'bz' : 'Belize',
+           'bj' : 'Benin',
+           'bt' : 'Bhutan',
+           'bo' : 'Bolivia',
+           'ba' : 'Bosnia and Herzegovina',
+           'bw' : 'Botswana',
            'br' : 'Brazil',
+           'bn' : 'Brunei',
+           'bg' : 'Bulgaria',
+           'bf' : 'Burkina Faso',
+           'bi' : 'Burundi',
+           'kh' : 'Cambodia',
+           'cm' : 'Cameroon',
            'ca' : 'Canada',
-           'fi' : 'Finland'}
+           'cv' : 'Cape Verde',
+           'cf' : 'Central African Republic',
+           'td' : 'Chad',
+           'cl' : 'Chile',
+           'cn' : 'China',
+           'co' : 'Colombia',
+           'km' : 'Comoros',
+           'cg' : 'Republic of the Congo',
+           'cd' : 'Democratic Republic of Congo',
+           'cr' : 'Costa Rica',
+           'ci' : "Côte d'Ivoire",
+           'hr' : 'Croatia',
+           'cu' : 'Cuba',
+           'cy' : 'Cyprus',
+           'cz' : 'Czechia',
+           'dk' : 'Denmark',
+           'dj' : 'Djibouti',
+           'dm' : 'Dominica',
+           'do' : 'Dominican Republic',
+           'ec' : 'Ecuador',
+           'eg' : 'Egypt',
+           'sv' : 'El Salvador',
+           'gq' : 'Equatorial Guinea',
+           'er' : 'Eritrea',
+           'ee' : 'Estonia',
+           'sz' : 'Eswatini',
+           'et' : 'Ethiopia',
+           'fj' : 'Fiji',
+           'fi' : 'Finland',
+           'fr' : 'France',
+           'ga' : 'Gabon',
+           'gm' : 'Gambia',
+           'ge' : 'Georgia',
+           'de' : 'Germany',
+           'gh' : 'Ghana',
+           'gr' : 'Greece',
+           'gd' : 'Grenada',
+           'gt' : 'Guatemala',
+           'gn' : 'Guinea',
+           'gw' : 'Guinea-Bissau',
+           'gy' : 'Guyana',
+           'ht' : 'Haiti',
+           'hn' : 'Honduras',
+           'hu' : 'Hungary',
+           'is' : 'Iceland',
+           'in' : 'India',
+           'id' : 'Indonesia',
+           'ir' : 'Iran',
+           'iq' : 'Iraq',
+           'ie' : 'Ireland',
+           'il' : 'Israel',
+           'it' : 'Italy',
+           'jm' : 'Jamaica',
+           'jp' : 'Japan',
+           'jo' : 'Jordan',
+           'kz' : 'Kazakhstan',
+           'ke' : 'Kenya',
+           'ki' : 'Kiribati',
+           'kp' : 'North Korea',
+           'kr' : 'South Korea',
+           'kw' : 'Kuwait',
+           'kg' : 'Kyrgyzstan',
+           'la' : 'Laos',
+           'lv' : 'Latvia',
+           'lb' : 'Lebanon',
+           'ls' : 'Lesotho',
+           'lr' : 'Liberia',
+           'ly' : 'Libya',
+           'li' : 'Liechtenstein',
+           'lt' : 'Lithuania',
+           'lu' : 'Luxembourg',
+           'mg' : 'Madagascar',
+           'mw' : 'Malawi',
+           'my' : 'Malaysia',
+           'mv' : 'Maldives',
+           'ml' : 'Mali',
+           'mt' : 'Malta',
+           'mh' : 'Marshall Islands',
+           'mr' : 'Mauritania',
+           'mu' : 'Mauritius',
+           'mx' : 'Mexico',
+           'fm' : 'Micronesia',
+           'md' : 'Moldova',
+           'mc' : 'Monaco',
+           'mn' : 'Mongolia',
+           'me' : 'Montenegro',
+           'ma' : 'Morocco',
+           'mz' : 'Mozambique',
+           'mm' : 'Myanmar',
+           'na' : 'Namibia',
+           'nr' : 'Nauru',
+           'np' : 'Nepal',
+           'nl' : 'Netherlands',
+           'nz' : 'New Zealand',
+           'ni' : 'Nicaragua',
+           'ne' : 'Niger',
+           'ng' : 'Nigeria',
+           'mk' : 'North Macedonia',
+           'no' : 'Norway',
+           'om' : 'Oman',
+           'pk' : 'Pakistan',
+           'pw' : 'Palau',
+           'ps' : 'Palestine',
+           'pa' : 'Panama',
+           'pg' : 'Papua New Guinea',
+           'py' : 'Paraguay',
+           'pe' : 'Peru',
+           'ph' : 'Philippines',
+           'pl' : 'Poland',
+           'pt' : 'Portugal',
+           'qa' : 'Qatar',
+           'ro' : 'Romania',
+           'ru' : 'Russia',
+           'rw' : 'Rwanda',
+           'kn' : 'Saint Kitts and Nevis',
+           'lc' : 'Saint Lucia',
+           'vc' : 'Saint Vincent and the Grenadines',
+           'ws' : 'Samoa',
+           'sm' : 'San Marino',
+           'st' : 'São Tomé and Príncipe',
+           'sa' : 'Saudi Arabia',
+           'sn' : 'Senegal',
+           'rs' : 'Serbia',
+           'sc' : 'Seychelles',
+           'sl' : 'Sierra Leone',
+           'sg' : 'Singapore',
+           'sk' : 'Slovakia',
+           'si' : 'Slovenia',
+           'sb' : 'Solomon Islands',
+           'so' : 'Somalia',
+           'za' : 'South Africa',
+           'ss' : 'South Sudan',
+           'es' : 'Spain',
+           'lk' : 'Sri Lanka',
+           'sd' : 'Sudan',
+           'sr' : 'Suriname',
+           'se' : 'Sweden',
+           'ch' : 'Switzerland',
+           'sy' : 'Syria',
+           'tj' : 'Tajikistan',
+           'tz' : 'Tanzania',
+           'th' : 'Thailand',
+           'tl' : 'Timor-Leste',
+           'tg' : 'Togo',
+           'to' : 'Tonga',
+           'tt' : 'Trinidad and Tobago',
+           'tn' : 'Tunisia',
+           'tr' : 'Turkey',
+           'tm' : 'Turkmenistan',
+           'tv' : 'Tuvalu',
+           'ug' : 'Uganda',
+           'ua' : 'Ukraine',
+           'ae' : 'United Arab Emirates',
+           'gb' : 'United Kingdom',
+           'us' : 'United States',
+           'uy' : 'Uruguay',
+           'uz' : 'Uzbekistan',
+           'vu' : 'Vanuatu',
+           'va' : 'Vatican City',
+           've' : 'Venezuela',
+           'vn' : 'Vietnam',
+           'ye' : 'Yemen',
+           'zm' : 'Zambia',
+           'zw' : 'Zimbabwe'}
     loadFlags(dic)
 
     #Initializing Score and Lives
@@ -30,49 +221,3 @@ def game_loop(screen):
             p.quit()
 
     end_screen.end_screen(screen, score)
-    # while not exit_game:
-    #     if game_finish:
-    #         end_screen.end_screen(screen, score)
-
-    #     elif game_over:
-    #         pass
-
-    #     else:
-    #         for event in p.event.get():
-    #             if event.type == p.QUIT:
-    #                 exit_game = True
-
-    #         screen.fill("green")
-
-    #         for key, value in dic.items():
-    #             screen.blit(Flags[key], (130, 200))
-    #             #Displaying the score
-    #             display_text(screen, "Score: " + str(score), 11, 11, 55, "black")
-    #             display_text(screen, "Score: " + str(score), 10, 10, 55, "yellow")
-    #             p.display.update()
-
-    #             voice_thread = threading.Thread(target = listen_for_voice_input)
-    #             voice_thread.start()
-
-    #             voice_thread.join()
-
-    #             answer = listen_for_voice_input()
-    #             print(answer)
-    #             if(answer == value):
-    #                 print("Correct!")
-    #                 score += 1
-
-    #             elif answer.lower() == 'exit':
-    #                 exit_game = True
-    #                 break
-
-    #             else:
-    #                 score -= 1
-
-    #                 if score == 0:
-    #                     game_over = True
-    #                     break
-        
-    #     game_finish = True
-    #     p.display.update()
-    #     clock.tick(fps)
