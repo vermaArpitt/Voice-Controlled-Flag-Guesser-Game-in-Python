f76ab488f3f920019a20abcedfc246103956f497
Arpit Verma <131401678+vermaArpitt@users.noreply.github.com>
2024-11-05 20:56:13 +0530

    Added display for voice input

--- game_level.py
+++ game_level.py
@@ -1,6 +1,7 @@
 import pygame as p
 import threading
 import sys
+import time
 from main import display_text, listen_for_voice_input
 
 p.init()
@@ -26,9 +27,14 @@ def game_level(screen, score, lives, flag, value):
     #Displaying the flag
     screen.blit(flag, (300, 200))
     
+    #Displaying prompt
+    display_text(screen, "Your Answer: ", 14, 351, 55, "black")
+    display_text(screen, "Your Answer: ", 10, 347, 55, "white")
+
     exit_game = False
     answer = None
     result = []
+    answer_display_time = None
 
     voice_thread = threading.Thread(target = listen_for_voice_input, args=(result,))
     voice_thread.start()
@@ -41,17 +47,19 @@ def game_level(screen, score, lives, flag, value):
                 p.quit()
                 sys.exit()
 
-        
-        if not voice_thread.is_alive() and len(result) != 0:  # Check if the thread has finished and result is not yet processed
-            # answer = listen_for_voice_input()
+        if not voice_thread.is_alive() and len(result) != 0:  # Checking if the thread has finished
             answer = result.pop(0)
-            print(answer)
-            if answer == value:
-                return score + 1, lives
+            answer_display_time = time.time()  # Setting the current time for displaying the answer
 
+            # Displaying the answer text 
+            display_text(screen, answer, 275, 347, 55, "white")
+            print(answer)
+
+        if answer_display_time and (time.time() - answer_display_time) > 3: #Added 3 second delay for displaying answer  
+            if answer.lower() == value:
+                return score + 1, lives
             elif answer.lower() == 'exit':
                 return -2, -2
-
             else:
                 return score, lives - 1
 
